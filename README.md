c9859a5643da6da76f4deb8a94b2546a00f1e36d
jkarstrom <44649059+jkarstrom@users.noreply.github.com>
2023-10-28 22:44:23 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Recipe App
+ Used React Native and other things :)
---
+++ src/helpers/image.js
@@ -0,0 +1,11 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useEffect, useState } from 'react';
+
+import Animated from 'react-native-reanimated';
+import { Image } from 'expo-image';
+
+export const CachedImage = (props) => {
+    const { uri } = props;
+  
+    return <Image source={uri} {...props} />;
+  };
